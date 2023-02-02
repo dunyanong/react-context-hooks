53433fc7b31bdb2979880502d73a07e08aef8b0e
Ong Dun Yan <ongdunyan@Ongs-Air.realtek>
2023-02-02 17:42:20 +0800

    first commit

---
+++ hooks/src/components/SongList.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'; 
+import { v4 as uuidv4 } from 'uuid'
+import NewSongForm from './NewSongForm';
+
+const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1 },
+        { title: 'memory gospel', id: 2 },
+        { title: 'this wild darkness', id: 3 }
+    ]);
+
+    const addSong = (title) => {
+        setSongs([...songs, {title: title, id: uuidv4()}]);
+    }
+
+    useEffect(() => {
+        console.log('useEffect hook ran', songs);
+    })
+
+    return (
+        <div className="song-list">
+            <ul>
+                {songs.map((song) => {
+                    return (
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
+            </ul>
+            <NewSongForm addSong={addSong}/>
+        </div>
+    );
+}
+ 
+export default SongList;
--- lesson.ts
+++ lesson.ts
@@ -23,14 +23,14 @@ export const ThemeContext = createContext();
 
 Inside sfc:
 const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState({
+    const [themeContext, setThemeContext] = useState({
         isLightTheme: true,
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
     });
     
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeContext.Provider value={{ themeContext, setThemeContext }}>
             {children}
         </ThemeContext.Provider>
     );
@@ -53,6 +53,168 @@ function App() {
     </div>
   );
 }
+
+Beware!
+ThemeContext is the object that holds the theme data
+ThemeContextProvider is the component that provides that data to the rest of the application.
+
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #4 - Accessing Context (part 1)
+-------------------------------------------------------------------------------------------------------------------------
+import { ThemeContext } from "../contexts/ThemeContext";
+
+const Navbar = () => {
+    const { themeContext, setThemeContext } = useContext(ThemeContext);    
+    console.log(theme);
+    return (                
+        <nav>
+            <h1>Context App</h1>            
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+The useContext hook takes in the context object that you imported from the ThemeContext file.
+It returns an object containing the current context value, in this case, theme and setTheme.
+You can then use this value to update the layout/styling of the component or log it to the console.
+
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #4 - Accessing Context (part 2)
+-------------------------------------------------------------------------------------------------------------------------
+const Navbar = () => {
+    <ThemeContext.Consumer>
+        {({ themeContext, setThemeContext }) => {
+            const { isLightTheme, light, dark } = theme;
+            const themeObject = isLightTheme ? light : dark;
+            
+            return (                
+                <nav style={{backgroundColor: themeObject.ui, color: themeObject.syntax }}>
+                    <h1>Context App</h1>            
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+      );
+        }}
+    </ThemeContext.Consumer>
+}
+
+This replaces the useContext hooks! 
+With this trick, you need the use another return() for the 2nd, 3rd so on.
+
+This is because each component that needs to access the context needs to be wrapped with the ThemeContext.Consumer component, which returns the JSX for that component.
+
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #6 - Updating Context Data
+-------------------------------------------------------------------------------------------------------------------------
+To change the data of state, like this one:
+const [themeContext, setThemeContext] = useState({
+    isLightTheme: true,
+    light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+    dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
+});
+
+const toggleTheme = () => {
+    setThemeContext(prevState => ({
+        ...prevState,
+        isLightTheme: !prevState.isLightTheme
+    }));
+}
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #7 - Creating Multiple Contexts
+-------------------------------------------------------------------------------------------------------------------------
+Similar tricks, create another file in the contexts directory:
+
+//
+import React, { useState, createContext } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+    const [isAuthenticated, setIsAutheticated] = useState({
+        isAuthenticated: false
+    });
+    const toggleAuth = () => {
+        setIsAutheticated( prevState => ({
+            ...prevState,
+            isAuthenticated: !prevState.isAuthenticated
+        }));
+    }
+    return (
+        <div>
+        <AuthContext.Provider value={{ isAuthenticated, setIsAutheticated, toggleAuth }}>
+            {children}
+        </AuthContext.Provider>
+        </div>
+    );
+}
+ 
+export default AuthContextProvider;
+//
+
+Next, remember to update the app.js:
+<ThemeContextProvider>
+    <AuthContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle/>
+    </AuthContextProvider> 
+</ThemeContextProvider> 
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #8 - Consuming Multiple Contexts
+-------------------------------------------------------------------------------------------------------------------------
+<AuthContext.Consumer>
+{(authContext) => (
+<ThemeContext.Consumer>
+    {({ themeContext }) => {
+    const { isAuthenticated, toggleAuth } = authContext;
+    const { isLightTheme, light, dark } = themeContext;
+    const themeObject = isLightTheme ? light : dark;        
+    return (                
+        <nav style={{backgroundColor: themeObject.ui, color: themeObject.syntax }}>
+            <h1>Context App</h1>    
+            <div onClick={toggleAuth}>
+            { isAuthenticated ? 'Logged in' : 'Logged out'}  
+            </div>        
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+    }}
+</ThemeContext.Consumer>
+)}
+</AuthContext.Consumer>
+
+-------------------------------------------------------------------------------------------------------------------------
+React Context & Hooks Tutorial #9 - Intro to Hooks
+-------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
